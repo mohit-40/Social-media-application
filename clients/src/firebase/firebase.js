c8051cd2d8b3becf324c3fd3c66584ba8bded13c
Mohit agrawal <2019285@iiitdmj.ac.in>
2021-09-12 00:49:19 +0530

    added firebase storage

---
+++ clients/src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+const firebaseConfig = {
+	apiKey: "AIzaSyDG4UwInJWCmztkK2nAIhqrLPJB5-H_FU4",
+	authDomain: "socialify-8b5dd.firebaseapp.com",
+	projectId: "socialify-8b5dd",
+	// databaseURL: "https://fir-react-upload-5daa4.firebaseio.com",
+	storageBucket: "socialify-8b5dd.appspot.com",
+	messagingSenderId: "106617260750",
+	appId: "1:106617260750:web:e0806af0c47ae445d59e9f",
+	measurementId: "G-PTGDWZE2HZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+export { storage, firebase as default };
