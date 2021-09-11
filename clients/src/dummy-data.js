b1d2e7e4910a104d84528c7c3ff667385c649d07
Mohit agrawal <2019285@iiitdmj.ac.in>
2021-09-11 14:01:46 +0530

    revert to commit <c7b79f48d73da1273472172b73d5236a540b90c3> due to anomaly error

---
+++ clients/src/dummy-data.js
@@ -0,0 +1,52 @@
+export const Users = [
+	{
+	  id:1,
+	  profilePicture: "person/1.jpeg",
+	  username: "Safak Kocaoglu",
+	},
+	{
+	  id:2,
+	  profilePicture: "person/2.jpeg",
+	  username: "Janell Shrum",
+	},
+	{
+	  id:3,
+	  profilePicture: "person/3.jpeg",
+	  username: "Alex Durden",
+	},
+	{
+	  id:4,
+	  profilePicture: "person/4.jpeg",
+	  username: "Dora Hawks",
+	},
+	{
+	  id:5,
+	  profilePicture: "person/5.jpeg",
+	  username: "Thomas Holden",
+	},
+	{
+	  id:6,
+	  profilePicture: "person/6.jpeg",
+	  username: "Shirley Beauchamp",
+	},
+	{
+	  id:7,
+	  profilePicture: "person/7.jpeg",
+	  username: "Travis Bennett",
+	},
+	{
+	  id:8,
+	  profilePicture: "person/8.jpeg",
+	  username: "Kristen Thomas",
+	},
+	{
+	  id:9,
+	  profilePicture: "person/9.jpeg",
+	  username: "Gary Duty",
+	},
+	{
+	  id:10,
+	  profilePicture: "person/10.jpeg",
+	  username: "Safak Kocaoglu",
+	},
+  ]; 
