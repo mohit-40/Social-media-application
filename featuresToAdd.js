8ea2ef8cd77ab60f07b0015586c9b5e5b8dd6931
Mohit agrawal <2019285@iiitdmj.ac.in>
2021-12-23 17:38:25 +0530

    a

---
+++ featuresToAdd.js
@@ -0,0 +1,7 @@
+// add follower/follwings redux as they are connected  done
+// online friend problem and dynamic	done	
+// make it dynamic	done
+Notification
+responsive
+
+delete message and conversation
--- step.js
+++
@@ -1,6 +0,0 @@
-// add follower/follwings redux as they are connected 
-// online friend problem and dynamic
-make it dynamic
-Notification
-responsive
-deploy
