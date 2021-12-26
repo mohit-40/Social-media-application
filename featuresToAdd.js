3dfb2cd87508a8f92c0b59f96b5ba3b7ced7019c
Mohit agrawal <2019285@iiitdmj.ac.in>
2021-12-27 01:58:25 +0530

    "notification style change"

--- featuresToAdd.js
+++ featuresToAdd.js
@@ -12,13 +12,13 @@ which db model is good for like=> { postId, like []} or {userId , postId }
 // style css
 // nodemailer
 // register login and user detail
+// responsive
 
 
-responsive
 delete message and conversation
 delete user  (CHALLENGE = then have to delete from all following, follower, like , comment, message)
 delete img from firebase storage
-notification backend and notification style
+notification backend
 birthday section
 multiple images
 label location feeling 
